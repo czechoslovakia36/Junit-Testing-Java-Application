5282e2a9abc3818521da3b27b371c9d09c5e66a1
Vivek Kumar <clocktower77@protonmail.com>
2023-02-20 15:13:41 +0530

    first commit

---
+++ src/test/java/com/programming/techie/ContactManagerTest.java
@@ -0,0 +1,54 @@
+package com.programming.techie;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ContactManagerTest {
+    @Test
+    @DisplayName("Should create Contact")
+    public void shouldCreateContact() {
+
+        ContactManager contactManager = new ContactManager();
+        contactManager.addContact("John", "Doe", "0123456789");
+
+//       Postive Test Case
+        Assertions.assertFalse(contactManager.getAllContacts().isEmpty());
+        Assertions.assertEquals(1, contactManager.getAllContacts().size());
+    }
+        @Test
+        @DisplayName("Should Not Create Contact When First Name is Null")
+        public void shouldThrowRuntimeExceptionWhenFirstNameIsNull(){
+            ContactManager contactManager= new ContactManager();
+            contactManager.addContact("John","Doe","0123456789");
+            Assertions.assertThrows(RuntimeException.class,() -> {
+                contactManager.addContact(null,"Doe","0123456789");
+            });
+
+        }
+
+    @Test
+    @DisplayName("Should Not Create Contact When Last Name is Null")
+    public void shouldThrowRuntimeExceptionWhenLastNameIsNull() {
+        ContactManager contactManager = new ContactManager();
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            contactManager.addContact("John", null, "0123456789");
+        });
+    }
+
+    @Test
+    @DisplayName("Should Not Create Contact When Phone Number is Null")
+    public void shouldThrowRuntimeExceptionWhenPhoneNumberIsNull() {
+        ContactManager contactManager = new ContactManager();
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            contactManager.addContact("John", "Doe", "0123456789");
+        });
+    }
+
+
+
+
+  
+}
+
